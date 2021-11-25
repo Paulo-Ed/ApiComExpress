c5ebc398fadf32252f7491bfb5794aece379dab4
Paulo Santos <paulo.eduardo@nutrin.com.br>
2021-11-25 10:13:48 -0300

    Definindo novos métodos http

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,11 +1,32 @@
+const atendimentos = require('../models/atendimentos')
 const Atendimento = require('../models/atendimentos')
 
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send("Rota de atendimentos! realizando novo get"))
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.lista(res)
+    })
+
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        Atendimento.buscaPorId(id, res)
+    })
 
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body
-        Atendimento.Adiciona(atendimento)
-        res.send("Rota de atendimentos! Fazendo um post")
+        Atendimento.Adiciona(atendimento, res)
+        
+    })
+
+    app.patch('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+
+        Atendimento.altera(id, valores, res)
+    })
+
+    app.delete('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req. params.id)
+
+        Atendimento.apaga(id, res)
     })
 }
